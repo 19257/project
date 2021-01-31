801c40dc05a9ac5146b61f9fa709eb14cfc3b914
19257 <zm19257@josai.ac.jp>
2021-01-31 12:31:06 +0000

    0420

---
+++ omikuji.js
@@ -0,0 +1,7 @@
+function omikuji(){
+ var kuzi = ["1","2","3","4","5"];
+ var c = coin[Math.floor(Math.random()*kuzi.length)];
+ if (c=1){
+ document.getElementById('daikchi').src ="omikuji_daikichi.png";
+ }
+}

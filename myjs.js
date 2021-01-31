d4f1caf315105914eace0dc95b930ed048e9e77b
19257 <zm19257@josai.ac.jp>
2021-01-31 07:20:48 +0000

    0421

--- myjs.js
+++ myjs.js
@@ -1,5 +1,11 @@
 function cointoss() {
- var arr = ["裏","表"];
- var a = arr[Math.floor(Math.random()*arr.length)];
+ var coin = ["裏","表"];
+ var a = coin[Math.floor(Math.random()*coin.length)];
  alert(a +"が出ました！");
+}
+
+function dice() {
+ var dice = ["1","2","3","4","5","6"];
+ var b = dice[Math.floor(Math.random()*dice.length)];
+ alert(b +"が出ました！");
 }

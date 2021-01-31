0bfe18d7f77b3413320aada0c97d2f970942ab98
19257 <zm19257@josai.ac.jp>
2021-01-31 16:32:15 +0000

    0420

--- omikuji.js
+++ omikuji.js
@@ -1,23 +1,20 @@
-function omikuji() {
- alert("test");
-}
-
-
-/*
-function omikuji() {
+ function kuji() {
  var kuji = ["1","2","3","4","5"];
  var c = kuji[Math.floor(Math.random()*kuji.length)];
- alert(c +"が出ました！");
-}
-*/
 
- /*
- if (c=1){
+ if (c==1){
  document.getElementById('kuji').src ="./omikuji/omikuji_daikichi.png";
  }
-}
-
-/*
-function omikuji(){
- document.getElementById('kuji').src ="./omikuji/omikuji_daikichi.png";
+  else if (c==2){
+  document.getElementById('kuji').src ="./omikuji/omikuji_chuukichi.png";
+  }
+   else if (c==3){
+   document.getElementById('kuji').src ="./omikuji/omikuji_kichi.png";
+   }
+    else if (c==4){
+    document.getElementById('kuji').src ="./omikuji/omikuji_suekichi.png";
+    }
+     else if (c==5){
+     document.getElementById('kuji').src ="./omikuji/omikuji_syoukichi.png";
+     }
 }
